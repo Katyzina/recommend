52fb02567e1c026649adb90f19e8bc8adc598ec6
unknown <razarenov.aleksandar@yandex.ru>
2024-03-09 15:08:43 +0300

    work

--- project/recommendation/urls.py
+++ project/recommendation/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path
 
-from recommendation.views import HomePageView, login_view, hrlog_view, regstud_view, support_view, employerlist_view, studprofile_view
+from recommendation.views import (
+    HomePageView, UserLoginView, EmployerRegisterView,
+    StudentRegisterView, SupportCreateView, EmployerListView,
+    StudentProfileView, logout_user
+)
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
-    path('login/', login_view, name='login'),
-    path('hrlog/', hrlog_view, name='hrlog'),
-    path('regstud/', regstud_view, name='regstud'),
-    path('support/', support_view, name='support'),
-    path('employerlist/', employerlist_view, name='employerlist'),
-    path('studprofile/', studprofile_view, name='studprofile'),
+    path('<int:institute_id>/', HomePageView.as_view(), name='filter_vacation'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', logout_user, name='logout'),
+    path('hrlog/', EmployerRegisterView.as_view(), name='hrlog'),
+    path('regstud/', StudentRegisterView.as_view(), name='regstud'),
+    path('support/', SupportCreateView.as_view(), name='support'),
+    path('employerlist/', EmployerListView.as_view(), name='employerlist'),
+    path('studprofile/', StudentProfileView.as_view(), name='studprofile'),
 ]
--- project/recommendation/views.py
+++ project/recommendation/views.py
@@ -1,41 +1,110 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
 from django.views import View
 
-from tables_app.models import Institute, Vacation
+from tables_app.models import Institute, Vacation, Student, User, Employer
 
 
 class HomePageView(View):
 
-    def get(self, request):
-        # get(id=1) -> obj
-        # filter(name=ep) -> [obj]
-        # all() -> [obj, obj]
+    def get(self, request, *args, **kwargs):
         institutes = Institute.objects.all()
         vacations = Vacation.objects.all()
+        if kwargs.get('institute_id'):
+            vacations = vacations.filter(institute_id=kwargs['institute_id'])
         return render(request, 'index.html', context={
             "institutes": institutes,
             "vacations": vacations,
         })
 
 
-def login_view(request):
-    return render(request, 'login.html')
+class UserLoginView(View):
+
+    def get(self, request):
+        return render(request, 'login.html')
+
+    def post(self, request):
+        user = authenticate(
+            request,
+            username=request.POST['email'],
+            password=request.POST['password'],
+        )
+        if user:
+            login(request, user)
+            return redirect('home')
+        return redirect('login')
 
 
-def hrlog_view(request):
-    return render(request, 'hrlog.html')
+class EmployerRegisterView(View):
+    def get(self, request):
+        return render(request, 'hrlog.html')
+
+    def post(self, request):
+        try:
+            employer_info = Employer.objects.create(
+                organization=request.POST['organization'],
+            )
+            User.objects.create_user(
+                username=request.POST['email'],
+                password=request.POST['password'],
+                first_name=request.POST['first_name'],
+                last_name=request.POST['last_name'],
+                middle_name=request.POST['middle_name'],
+                phone_number=request.POST['phone_number'],
+                email=request.POST['email'],
+                employer=employer_info,
+            )
+        except Exception as error:
+            return redirect('regstud')
+        else:
+            return redirect('login')
 
 
-def regstud_view(request):
-    return render(request, 'regstud.html')
+class StudentRegisterView(View):
+    def get(self, request):
+        institutes = Institute.objects.all()
+        return render(request, 'regstud.html', context={
+            'institutes': institutes
+        })
 
-def support_view(request):
-    return render(request, 'support.html')
+    def post(self, request):
+        try:
+            student_info = Student.objects.create(
+                institute_id=request.POST['institute'],
+                study_group=request.POST['study_group']
+            )
+            User.objects.create_user(
+                username=request.POST['email'],
+                password=request.POST['password'],
+                first_name=request.POST['first_name'],
+                last_name=request.POST['last_name'],
+                middle_name=request.POST['middle_name'],
+                phone_number=request.POST['phone_number'],
+                email=request.POST['email'],
+                student=student_info,
+            )
+        except Exception as error:
+            return redirect('regstud')
+        else:
+            return redirect('login')
 
-def employerlist_view(request):
-    return render(request, 'employerlist.html')
 
-def studprofile_view(request):
-    return render(request, 'studprofile.html')
+class SupportCreateView(View):
+    def get(self, request):
+        return render(request, 'support.html')
 
 
+class EmployerListView(View):
+
+    def get(self, request):
+        return render(request, 'employerlist.html')
+
+
+class StudentProfileView(View):
+    def get(self, request):
+        return render(request, 'studprofile.html')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
